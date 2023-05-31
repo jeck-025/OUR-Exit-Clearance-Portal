29f0705527d858aa784ef00669f575ff308db5b3
jeck-025 <jerrickanatalio@gmail.com>
2023-05-31 10:59:07 +0800

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -9,6 +9,7 @@ $view = new view();
 
     <script src="https://kit.fontawesome.com/03ca298d2d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap-select.min.css">
     <link rel="stylesheet" href="resource/css/transfer.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 
@@ -107,7 +108,7 @@ $view = new view();
               <!---Course/Degree--->
               <div class="col-md-8">
                 <label form="course" class="form-label">Course/Degree</label>
-                <select id="course" name="course" class="form-select form-control" data-live-search="true" required>
+                <select id="course" name="course" class="form-select form-control selectpicker" data-live-search="true" required>
                 <?php $view->courseSP2();?>
                 </select>
               </div>
@@ -127,7 +128,7 @@ $view = new view();
               <!---Transfer School--->
               <div class="col-md-5">
                 <label for="university" class="form-label">Transferring School</label>
-                <select id="university" name="university" class="form-select form-control" data-live-search="true" required>
+                <select id="university" name="university" class="form-select form-control selectpicker" data-live-search="true" required>
                 <?php $view->university();?>
                 </select>
               </div>
@@ -195,6 +196,7 @@ $view = new view();
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="vendor/js/bootstrap-select.min.js"></script>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
