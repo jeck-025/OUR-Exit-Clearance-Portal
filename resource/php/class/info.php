da0491907f1fcbf9cba6d0f0bd018048a3c02194
jeck-025 <jerrickanatalio@gmail.com>
2023-05-31 11:29:19 +0800

    Update info.php

--- resource/php/class/info.php
+++ resource/php/class/info.php
@@ -1471,7 +1471,7 @@ class info extends config{
                     }else{
                       echo "<a href='resource/uploads/ids/$attachedID' class='btn btn-primary btn-sm btn-block d-block' target='_blank'><i class='fa-solid fa-id-card'></i> View Attached ID</a>";
                     }
-                    if(empty($attachedID)){
+                    if(empty($attachedLTR)){
                       echo "<a href='resource/uploads/letters/$attachedLTR' class='btn btn-primary btn-sm btn-block d-block' target='_blank'><i class='fa-solid fa-envelope'></i> View Letter of Intent for Exit</a>";
                     }else{
                       echo "<a href='resource/uploads/letters/$attachedLTR' class='btn btn-primary btn-sm btn-block d-block' target='_blank'><i class='fa-solid fa-envelope'></i> View Letter of Intent for Exit</a>";
