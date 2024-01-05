42739f21d3daa87f24513477fee6bd43bfc3caad
jeck-025 <jerrickanatalio@gmail.com>
2024-01-05 10:19:57 +0800

    update 71.2

--- resource/php/functions/funct.php
+++ resource/php/functions/funct.php
@@ -14,6 +14,7 @@ function Success(){
     echo '<div class="alert alert-success alert-dismissible fade show col-12" role="alert">
             <b>Congratulations!</b> You have successfully added a new user account.
             <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
             </button>
         </div>';
     }
